c6da40bc3b9eab5c592ae85e707f48724cf3b46d
gary909 <garywhite61@hotmail.com>
2023-03-13 12:28:18 +0100

    code

---
+++ app.js
@@ -0,0 +1,15 @@
+function trueOrFalse(val){
+    if ()    return ?;             
+    else     return ?;
+}
+
+console.log(trueOrFalse(a>b));// "false";
+console.log(trueOrFalse(a===b));// "false";
+console.log(trueOrFalse(a<b));// "true";
+console.log(trueOrFalse(a!==b));// "true";
+console.log(trueOrFalse(b>c));// "true";
+console.log(trueOrFalse(b===c));// "false";
+console.log(trueOrFalse(b<c));// "false";
+console.log(trueOrFalse(b!==c));// "true";
+console.log(trueOrFalse(a===c));// "true";
+console.log(trueOrFalse(a!==c));// "false";
